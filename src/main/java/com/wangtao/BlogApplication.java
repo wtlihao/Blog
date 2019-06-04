41333a36c68611c28e8ef15be02d02b3573d645a
wangtao <lihao@fanhaoyue.com>
2019-06-04 11:46:26 +0800

    init

---
+++ src/main/java/com/wangtao/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.wangtao;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableWebMvc
+@EnableScheduling
+public class BlogApplication {
+}
